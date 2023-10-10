b85689bd41a063e98dc58c1270f65a91b22c11a4
SardorNigmatov <sardornigmatov2003@gmail.com>
2023-10-10 18:44:58 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get("SECRET_KEY")
+# SECRET_KEY = config.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-6_7s!5nt04sxn9h8k&1pv2=1az^$6qsjz3nxa*z3oi+lzs&y+0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,14 +88,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config.get("NAME"),
+#         'USER': config.get("USER"),
+#         'PASSWORD': config.get("PASSWORD"),
+#         'HOST':config.get("HOST"),
+#         'PORT':config.get("PORT"),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config.get("NAME"),
-        'USER': config.get("USER"),
-        'PASSWORD': config.get("PASSWORD"),
-        'HOST':config.get("HOST"),
-        'PORT':config.get("PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
